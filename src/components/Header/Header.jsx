c8ab5b2d203d1b1ee92533e0734cf3147ecdd1af
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2023-04-20 09:07:20 +0200

    add navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../../img/logo.png";
+import { Link } from "react-router-dom";
 
 //import style.css
 import "../Header/style.css";
@@ -8,8 +9,23 @@ export default function Header() {
   return (
     //il faut faire un hader avec une navbar avec liens vers pages du site
     <div className="headerContainer">
-      <div className="logo">
-        <img src={logo} alt="logo" />
+      <div className="headerContainerMinColumn">
+        <div className="firstHeader">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div>ici sera la searchbar</div>
+        </div>
+
+        <div className="navBar">
+          {/* on va mettre dans cette div la navbar */}
+          <Link to="/">
+            <p>News</p>
+          </Link>
+          {/* il faudra link vers la page cinéma, séries... */}
+          <p>Cinéma</p>
+          <p>Séries</p>
+        </div>
       </div>
     </div>
   );
